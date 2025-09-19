f8876980ce629ad477929245740ac3b40ac2bb0f
dmitriich <chdmitry2@gmail.com>
2025-09-19 17:30:21 +0300

    fix: make pipeline benchmark open

--- src/main/kotlin/benchmark/BenchmarkPipeline.kt
+++ src/main/kotlin/benchmark/BenchmarkPipeline.kt
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
 @State(Scope.Benchmark)
-class BenchmarkPipeline {
+open class BenchmarkPipeline {
     private val numThreads = 4
 
     @Param("1", "2", "4")
